219e75ba3632cdc99a2c2a228f1fe8453b592b87
Richard Russon <rich@flatcap.org>
2021-10-03 02:01:08 +0100

    whitespace

--- whitespace.sh
+++ whitespace.sh
@@ -1,34 +1,79 @@
 #!/bin/bash
 
-Tests
-	trailing whitespace
-	tabs in code
-	space tab
-	tailing blank line
-
 set -o errexit # set -e
 set -o nounset # set -u
 
 export LANG=C.UTF-8
 
-function test_whitespace()
+function summary()
 {
-	TEST="Trailing whitespace"
-	echo "$TEST"
-	if grep -lR --exclude-dir='.git' --exclude='*.png' --exclude='SECURITY.md' '[[:space:]]$'; then
-		SOFT_FAIL+=("$TEST")
-	fi
+	local TEST="$1"
+	local FILES="$2"
 
-	TEST="Tabs in code"
-	echo "$TEST"
-	if grep -lR --exclude='*' --include='*.[ch]' --exclude='jimsh0.c' --exclude='queue.h' '	'; then
-		SOFT_FAIL+=("$TEST")
-	fi
+	[ -z "$FILES" ] && return
 
-	TEST="Space-Tab"
 	echo "$TEST"
-	if grep -lR --exclude-dir='.git' --exclude='*.png' --exclude='jimsh0.c' --exclude='queue.h' ' \	'; then
-		SOFT_FAIL+=("$TEST")
-	fi
+	echo "$FILES" | sed 's/^/\t/'
+	echo
 }
 
+function trailing()
+{
+	local TEST="Trailing whitespace"
+	local FILES
+
+	FILES=$(grep -lR --exclude-dir='.git' --exclude='*.png' --exclude='SECURITY.md' '[[:space:]]$' || :)
+
+	summary "$TEST" "$FILES"
+}
+
+function tabs()
+{
+	local TEST="Tabs in code"
+	local FILES
+
+	FILES=$(grep -lR --exclude='*' --include='*.[ch]' --exclude='jimsh0.c' --exclude='queue.h' '	' || :)
+
+	summary "$TEST" "$FILES"
+}
+
+function space_tab()
+{
+	local TEST="Space-Tab"
+	local FILES
+
+	FILES=$(grep -lR --exclude-dir='.git' --exclude='*.png' --exclude='jimsh0.c' --exclude='queue.h' ' \	' || :)
+
+	summary "$TEST" "$FILES"
+}
+
+function trailing_line()
+{
+	local TEST="Trailing Blank Line"
+	local FILES=()
+	local TAIL
+	local i
+
+	for i in $(find . \( -name .git -o -name queue.h -o -path './autosetup/*' -o -path './docs/*' \) -prune -o -type f -name '*.[ch]' -print | sed 's/^\.\///' | sort); do
+		TAIL=$(tail -q -n1 $i)
+		if [ -z "$TAIL" ]; then
+			FILES+=("$i")
+		fi
+	done
+
+	IFS=$'\n'
+	summary "$TEST" "${FILES[*]}"
+}
+
+if [ $# -gt 0 ]; then
+	if [ ! -d "$1" ]; then
+		echo "error: '$1' is not a directory"
+		exit 1
+	fi
+	cd "$1"
+fi
+
+trailing
+tabs
+space_tab
+trailing_line
